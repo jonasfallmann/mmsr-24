6de6f3ebcda6b7e768dc889bd9e4619d10408fe5
Kristof Maar <kristof.maar@gmail.com>
2024-12-08 08:50:08 +0100

    add genres to baseline_script.py

--- baseline_script.py
+++ baseline_script.py
@@ -6,13 +6,14 @@ from tqdm import tqdm
 
 # %%
 class Track:
-    def __init__(self, track_id, track_name, artist, album_name, url, tfidf_vector):
+    def __init__(self, track_id, track_name, artist, album_name, url, tfidf_vector, genres):
         self.track_id = track_id
         self.track_name = track_name
         self.artist = artist
         self.album_name = album_name
         self.url = url
         self.tfidf_vector = tfidf_vector
+        self.genres = genres
       
     def __str__(self):
         return f'{self.track_id} - {self.track_name} - {self.artist} - {self.album_name}'
@@ -53,12 +54,13 @@ class BaselineIRSystem(IRSystem):
         remaining_tracks = [t for t in self.tracks if t.track_id != query.track_id]
         return np.random.choice(remaining_tracks, n, replace=False).tolist()
 
-def preprocess(basic_information: pd.DataFrame, youtube_urls: pd.DataFrame, tfidf_df: pd.DataFrame):
+def preprocess(basic_information: pd.DataFrame, youtube_urls: pd.DataFrame, tfidf_df: pd.DataFrame, genres_df: pd.DataFrame):
     basic_with_links = pd.merge(basic_information, youtube_urls, how="left", on="id")
     tracks = []
     for index, row in basic_with_links.iterrows():
         track_id = row['id']
         tfidf_vector = tfidf_df.loc[track_id].values if track_id in tfidf_df.index else None
+        genres = eval(genres_df.loc[track_id].genre) if track_id in genres_df.index else []
         
         track = Track(
             track_id,
@@ -66,7 +68,8 @@ def preprocess(basic_information: pd.DataFrame, youtube_urls: pd.DataFrame, tfid
             row['artist'],
             row['album_name'],
             row["url"],
-            tfidf_vector
+            tfidf_vector,
+            genres
         )
         tracks.append(track)
     return tracks
