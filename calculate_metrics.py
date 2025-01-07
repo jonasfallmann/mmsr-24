4b44c6aa3f9a577fadf88f54eb295136b636cc2a
Kroenik <niklas_kr@hotmail.de>
2025-01-07 16:09:13 +0100

    add evaluation based on popularity

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -50,7 +50,13 @@ class MRR(EvaluationMetric):
             if track in relevant_tracks:
                 return 1 / (idx + 1)
         return 0
-
+    
+class Popularity(EvaluationMetric):
+    def __init_(self):
+        pass
+    def evaluate(self, recommended_tracks):
+        return sum([track.popularity for track in recommended_tracks])/len(recommended_tracks)
+        
 class MetricsEvaluation(EvaluationProtocol):
     def __init__(self, tracks):
         self.tracks = tracks
@@ -60,6 +66,7 @@ class MetricsEvaluation(EvaluationProtocol):
         recall_scores = []
         ndcg_scores = []
         mrr_scores = []
+        popularity_scores = []
 
         for query_track in tqdm(self.tracks, desc="Evaluating IR system"):
             relevant_ids = [
@@ -77,22 +84,26 @@ class MetricsEvaluation(EvaluationProtocol):
             recall_metric = RecallAtK(k)
             ndcg_metric = NDCGAtK(k)
             mrr_metric = MRR()
+            popularity_metric = Popularity()
 
             precision = precision_metric.evaluate(recommended_ids, relevant_ids)
             recall = recall_metric.evaluate(recommended_ids, relevant_ids)
             ndcg = ndcg_metric.evaluate(recommended_ids, relevant_ids)
             mrr = mrr_metric.evaluate(recommended_ids, relevant_ids)
+            popularity = popularity_metric.evaluate(recommended_tracks, True)
 
             precision_scores.append(precision)
             recall_scores.append(recall)
             ndcg_scores.append(ndcg)
             mrr_scores.append(mrr)
+            popularity_scores.append(popularity)
 
         results = {
             "Precision@10": np.mean(precision_scores),
             "Recall@10": np.mean(recall_scores),
             "NDCG@10": np.mean(ndcg_scores),
             "MRR": np.mean(mrr_scores),
+            "Popularity": np.mean(popularity_scores),
         }
         return results
     
@@ -102,8 +113,10 @@ if __name__ == "__main__":
     tfidf_df = pd.read_csv("dataset/id_lyrics_tf-idf_mmsr.tsv", sep='\t', index_col=0)
     genres_df = pd.read_csv("dataset/id_genres_mmsr.tsv", sep='\t', index_col=0)
     tags_df = pd.read_csv("dataset/id_tags_dict.tsv", sep='\t')
+    spotify_df = pd.read_csv('dataset/id_metadata_mmsr.tsv', sep='\t')
+    lastfm_df = pd.read_csv('dataset/id_total_listens.tsv', sep='\t')
 
-    tracks = preprocess(basic_info_df, youtube_urls_df, tfidf_df, genres_df, tags_df)
+    tracks = preprocess(basic_info_df, youtube_urls_df, tfidf_df, genres_df, tags_df, spotify_df, lastfm_df)
     baseline_ir = BaselineIRSystem(tracks)
     print("Baseline IR System")
     text_ir = TextIRSystem(tracks)
